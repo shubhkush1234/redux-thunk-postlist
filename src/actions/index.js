4aedd7eb26e2b2d1639cb04d61de8b5cc57d66a0
shubham kushwaha <shubhkush1234@gmail.com>
2021-09-14 01:49:15 +0530

    added axios instance and bad approach for action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../services/jsonPlaceholder";
+
+//jsonPlaceholder is the axios instance which we created in services.
+
+export const fetchPosts = async () => {
+    const response = await jsonPlaceholder.get('/posts');
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
 };
+

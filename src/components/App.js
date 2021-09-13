eb295a36cbc887ab0d1075d151effad4918b15aa
shubham kushwaha <shubhkush1234@gmail.com>
2021-09-13 12:38:15 +0530

    added postList and redux setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PostList from './PostList';
+
+const App = () => {
+        return (
+            <div className="ui container">
+                Hi
+                <PostList/>
+            </div>
+        );
+};
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostList extends Component{
+    render(){
+        return(
+            <div>Postlist comp</div>
+        )
+    }
+}
+
+export default PostList;
